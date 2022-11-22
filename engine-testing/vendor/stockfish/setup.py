61eadd7f37b08f6a07782b5d23f7777e32840776
Kieran Wood <kieranw098@gmail.com>
2022-11-22 16:28:43 -0700

    updated entine with vendor setup

---
+++ engine-testing/vendor/stockfish/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="stockfish",
+    author="Ilya Zhelyabuzhsky",
+    author_email="zhelyabuzhsky@icloud.com",
+    version="3.28.0",
+    license="MIT",
+    keywords="chess stockfish",
+    python_requires=">=3.7",
+    url="https://github.com/zhelyabuzhsky/stockfish",
+    description="Wraps the open-source Stockfish chess engine for easy integration into python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=["stockfish", "stockfish.*"]),
+    install_requires=[],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-cov"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Games/Entertainment :: Board Games",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+)
