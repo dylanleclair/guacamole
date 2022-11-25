3c087ce720f041904dd1ab621c328b6e132ca22a
dylanleclair <dylan.leclair@icloud.com>
2022-11-24 17:11:17 -0700

    Update next.config.js
    Ignore node modules so that polling doesn't melt WSL slow i/o LOL

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   // except for webpack, other parts are left as generated
   webpack: (config, context) => {
     config.watchOptions = {
-      poll: 1000,
+      poll: 1500,
       aggregateTimeout: 300,
+      ignored: /node_modules/,
     };
     return config;
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
 };
 
